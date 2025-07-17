3d558f7f7c19472ea04c87ee54810f5d061c6e55
Enes Yaman <enesyaman04@gmail.com>
2025-07-17 16:31:12 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('themeToggle');
+button.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  button.textContent = document.body.classList.contains('dark') ? '☀️ Light Mode' : '🌙 Dark Mode';
+});
