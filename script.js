925dc66be6a5a042857bb5bffbe67831127afec5
EnesYaman04 <enesyaman04@gmail.com>
2025-07-18 15:08:12 +0300

    -Bg

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const body = document.body;
 const button = document.getElementById('themeToggle');
+
+// Açılışta dark mode aktif olsun
+body.classList.add('dark');
+button.textContent = '☀️ Light Mode';
+
+// Butona tıklanınca tema değiştir
 button.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  button.textContent = document.body.classList.contains('dark') ? '☀️ Light Mode' : '🌙 Dark Mode';
+    body.classList.toggle('dark');
+    button.textContent = body.classList.contains('dark') ? '☀️ Light Mode' : '🌙 Dark Mode';
 });
